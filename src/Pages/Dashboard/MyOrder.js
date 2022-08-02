a492ee9e6d1eff5f272e67989d89ff4b1155d1cc
abdullahboshir <https://github.com/abdullahboshir/t-shirt-react-router>
2022-08-02 12:33:36 +0600

    completed my order page

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -1,49 +1,40 @@
-import React from 'react';
-import { useAuthContext } from '../../Components/AuthContextProvider';
+import React, { useEffect, useState } from 'react';
+import MyOrderUi from './MyOrderUi';
+
 
 
 const MyOrder = () => {
+  const [confirmOrder, setConfirmOrder] = useState([]);
 
-    return (
-        <div className="overflow-x-auto mt-12 px-8">
-        <table className="table w-full">
-          {/* <!-- head --> */}
-          <thead>
-            <tr>
-              <th></th>
-              <th>Date</th>
-              <th>Total Prcie</th>
-              <th>Email</th>
-              <th>Tranzation Id</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* <!-- row 1 --> */}
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
-            {/* <!-- row 2 --> */}
-            <tr>
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-              <td>Purple</td>
-            </tr>
-            {/* <!-- row 3 --> */}
-            <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-              <td>Red</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
+  useEffect(() => {
+    fetch('http://localhost:5000/userOrderData')
+      .then(res => res.json())
+      .then(data => setConfirmOrder(data))
+  }, [confirmOrder, setConfirmOrder])
+
+
+  return (
+    <div className="overflow-x-auto mt-12 px-8">
+      <table className="table w-full">
+        {/* <!-- head --> */}
+        <thead>
+          <tr>
+            <th></th>
+            <th>Date</th>
+            <th>Total Prcie</th>
+            <th>Email</th>
+            <th>Tranzation Id</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            confirmOrder.map(order => <MyOrderUi index order={order} key={order._id} />)
+          }
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default MyOrder;
---
+++ src/Pages/Dashboard/MyOrderUi.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import swal from 'sweetalert';
+
+const MyOrderUi = ({order}) => {
+    const [willlDelet, setWillDelete] = useState({});
+
+    const {
+        OrderTotalPrice,
+        cutomerEmail,
+        date,
+        _id
+    } = order;
+
+
+const handleCancelOrder = (id) => {
+ swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("Poof! Your Order has been deleted!", {
+            icon: "success",
+          });
+          const url = `http://localhost:5000/userOrderData/${id}`;
+          fetch(url, {
+              method: 'DELETE'
+          })
+          .then(res => res.json())
+          .then(data => {
+              if(data.deletedCount > 0){
+                  const reamaining = order.filter(user => user._id !== id);
+                  setWillDelete(reamaining); 
+              }
+          })
+        } else {
+          swal("Your Order is safe!");
+        }
+      });
+       
+
+}
+
+    return (
+        <tr>
+        <td></td>
+        <td>{date}</td>
+        <td>${OrderTotalPrice}.00</td>
+        <td>{cutomerEmail}</td>
+        <td>Tranzation Id</td>
+        <td><button onClick={() => handleCancelOrder(_id)} className='text-secondary mr-4'>Cancel</button>
+        <button className='text-green-500 font-bold'>Payment</button>
+        </td>
+      </tr>
+    );
+};
+
+export default MyOrderUi;
